5af0497f617fa938421b2afe922a67b0332b760a
Ali <alihassnain330@gmail.com>
2023-01-26 14:37:22 +0500

    Output issue fixed!

---
+++ index.js
@@ -0,0 +1,11 @@
+var input_text = document.querySelector(".text_field");
+var translated_text = document.querySelector(".output-result");
+var btn = document.querySelector("button");
+
+
+btn.addEventListener("click",function(){
+    
+    translated_text.innerHTML= input_text.value;
+    // console.log(input_text);
+//    translated_text.innerHTML= 
+})
