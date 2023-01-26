d773877f0c7dabdeeb1901220625109c5d4d3787
Ali <alihassnain330@gmail.com>
2023-01-26 15:49:02 +0500

    Javascript with api calling working !

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+// Creating Variable & Targetting different class using DOM
 var input_text = document.querySelector(".text_field");
 var translated_text = document.querySelector(".output-result");
 var btn = document.querySelector("button");
 
 
+// Api Url configuration
+ var url = "https://api.funtranslations.com/translate/minion.json";
+function translator(texturl){
+ return url + "?" + "text=" + texturl;
+}
+
+function errorhandle(error){
+    console.log("Error : " + error);
+}
+
+
+
+// Click Event
 btn.addEventListener("click",function(){
+    var input = input_text.value;
     
-    translated_text.innerHTML= input_text.value;
-    // console.log(input_text);
-//    translated_text.innerHTML= 
+    // translated_text.innerHTML= input;
+    
+      fetch(translator(input))
+      .then(response=>response.json())
+     .then(json => translated_text.innerHTML= json.contents.translated)
+     .catch(errorhandle);
 })
